8bea574729cd0bab575d0429e7d190378d1031eb
Animesh Mondal <animeshmondal1414@gmail.com>
2023-09-28 19:12:03 +0530

    day_1

---
+++ day_1/app.js
@@ -0,0 +1,27 @@
+//! Day 1 Exercises
+
+//! 1.Write a single line comment which says, comments can make code readable
+//comments can make code readable
+//! 2.Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+/*
+    comments can make code readable and
+     easy to reuse and informative
+*/
+//! 3.declare variables and assign string, boolean, undefined and null data types
+let s = `this is a string`
+let b = true
+let u
+let n = null
+//! 4.use the JavaScript typeof operator to check different data types. Check the data type of each variable
+console.log(typeof (s));
+console.log(typeof (b));
+console.log(typeof (u));
+console.log(typeof (n));
+//! 5.Declare variables to store your first name, last name, marital status, country and age in a single line
+let f_name = `Animesh`, l_name = `Mondal`, marital_status = false, country = `INDIA`, age = 21
+console.log(`First Name: ${f_name}, Last Name: ${l_name}, Age: ${age}, Marital Status: ${marital_status}, Country: ${country}`);
+//! 6.Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+let myAge = 21
+let yourAge = 18
+console.log(`I am ${myAge} years old.`);
+console.log(`You Are ${yourAge} years old.`);
